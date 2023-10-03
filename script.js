4490c1c5c1bd127c67755d23d21f72525e9a4ed2
Noey26150 <146895545+Noey26150@users.noreply.github.com>
2023-10-04 01:29:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ document.getElementById("myButton").addEventListener("mouseover", function() {
         windowHeight = window.innerHeight,
         windowWidth = window.innerWidth;
 
-        this.style.top = Math.random() * (windowHeight - buttonHeight) + 'px';
-        this.style.left = Math.random() * (windowWidth - buttonWidth) + 'px';
+    this.style.top = Math.random() * (windowHeight - buttonHeight) + 'px';
+    this.style.left = Math.random() * (windowWidth - buttonWidth) + 'px';
+});
+
+document.getElementById("myButton").addEventListener("click", function() {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
 });
