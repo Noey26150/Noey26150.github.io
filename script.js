d35aae6aebe9f619a36d12e9222f4d939b0d2e6a
Noey26150 <146895545+Noey26150@users.noreply.github.com>
2023-10-04 01:35:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-document.getElementById("myButton").addEventListener("mouseover", function() {
-    var buttonHeight = this.offsetHeight,
-        buttonWidth = this.offsetWidth,
-        windowHeight = window.innerHeight,
-        windowWidth = window.innerWidth;
+var timeoutId;
 
-    this.style.top = Math.random() * (windowHeight - buttonHeight) + 'px';
-    this.style.left = Math.random() * (windowWidth - buttonWidth) + 'px';
+document.getElementById("myButton").addEventListener("mouseover", function() {
+    var button = this;
+    timeoutId = setTimeout(function() {
+        var buttonHeight = button.offsetHeight,
+            buttonWidth = button.offsetWidth,
+            windowHeight = window.innerHeight,
+            windowWidth = window.innerWidth;
+
+        button.style.top = Math.random() * (windowHeight - buttonHeight) + 'px';
+        button.style.left = Math.random() * (windowWidth - buttonWidth) + 'px';
+    }, 300); // 500ms delay
+});
+
+document.getElementById("myButton").addEventListener("mouseout", function() {
+    clearTimeout(timeoutId);
 });
 
 document.getElementById("myButton").addEventListener("click", function() {
     window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+});
+
+document.getElementById("myButton").addEventListener("focus", function(event) {
+    event.preventDefault();
 });
