8a095ffa8074b283e44a5cde0808226db9ef0ad4
Noey26150 <146895545+Noey26150@users.noreply.github.com>
2023-10-04 11:46:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.getElementById("myButton").addEventListener("mouseover", function() {
 
         button.style.top = Math.random() * (windowHeight - buttonHeight) + 'px';
         button.style.left = Math.random() * (windowWidth - buttonWidth) + 'px';
-    }, 200); // 200ms delay
+    }, 50); // 50ms delay
 });
 
 document.getElementById("myButton").addEventListener("mouseout", function() {
